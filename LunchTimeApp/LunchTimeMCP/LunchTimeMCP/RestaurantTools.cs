5f126cc07c2f29728ae9733a98599426bdb03272
Jake Kirkbride <jakelegendxiii@gmail.com>
2025-08-10 15:50:22 -0400

    Add Tools

---
+++ LunchTimeApp/LunchTimeMCP/LunchTimeMCP/RestaurantTools.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Text.Json;
+using ModelContextProtocol.Server;
+
+namespace LunchTimeMCP;
+
+[McpServerToolType]
+public sealed class RestaurantTools
+{
+	private readonly RestaurantService restaurantService;
+
+	public RestaurantTools(RestaurantService restaurantService)
+	{
+		this.restaurantService = restaurantService;
+	}
+
+	// All tools implemented here...
+}
